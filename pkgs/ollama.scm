23bc03821a95a3bc9faaa02292f3192d503a1b00
Jesse Eisses <jesse@eisses.email>
2026-02-09 12:32:27 +0100

    Add ollama binary package including rocm
    This does not properly patchelf all the libriaries that ship with
    ollama, as that appears a daunting amount of work. Ollama uses `.so`
    files relative the system path and not on the system (like many go
    programs).
    
    This means once the package is included, LD_LIBRARY_PATH must be
    properly populated. The easiest way to do this is by running it in a
    container with -F: `guix shell -CNF ollama`

---
+++ pkgs/ollama.scm
@@ -0,0 +1,86 @@
+(define-module (ollama)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages commencement)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages parallel)
+  #:use-module (guix download)
+  #:use-module (guix gexp)
+  #:use-module ((guix licenses) :prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix-science packages machine-learning)
+  #:use-module (nonguix build-system binary)
+  )
+
+(define ollama-rocm-source
+  (origin
+    (method url-fetch)
+    (uri
+     (string-append
+      "https://github.com/ollama/ollama/releases/download/v"
+      "0.15.5"
+      "/ollama-linux-amd64-rocm.tar.zst"))
+    (sha256
+     (base32
+      "0yylpvh9kswj0c31lbjhb72x959w9m9n18755pchkk3r40qkq7qh"))))
+
+(define-public ollama
+  (package
+    (name "ollama")
+    (version "0.15.5")
+    (source
+     (origin
+       (method url-fetch)
+       (uri
+	(string-append
+	 "https://github.com/ollama/ollama/releases/download/v"
+	 version
+	 "/ollama-linux-amd64.tar.zst"))
+       (sha256
+	(base32
+	 "09v89p6gz290aclxkdldncxmr8rjl1r0ppacwly183jlgd1awjff"))))
+    (build-system binary-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+	  (add-after 'binary-unpack 'unpack-rocm
+	    (lambda _
+	      (format #t "Current dir: ~a~%" (getcwd))
+
+	      (invoke "tar" "--zstd" "-xf"
+		      #$ollama-rocm-source
+		      "-C" ".."
+		      "--wildcards" "lib/*")
+	      (format #t "Lib after merge: ~a~%"
+		      (find-files "../lib/ollama" ".*"))))
+	  (add-before 'patchelf 'debug-patchelf-plan
+	    (lambda _
+	      (format #t "Found libraries: ~a~%"
+		      (find-files "../lib" ".*\\.so(\\.[0-9]+)*$")
+		      )))
+	  (add-after 'binary-unpack 'chmod-to-allow-patchelf
+	    (lambda _
+	      (chmod "ollama" #o755))))
+      #:strip-binaries? #f
+      #:patchelf-plan
+      #~(let ((libs (find-files "../lib" ".*\\.so(\\.[0-9]+)*$")))
+	  `(("ollama" ("gcc-toolchain"))))
+      #:validate-runpath? #f
+      #:install-plan
+      #~`(("ollama" "bin/ollama")
+	  ("../lib" "lib")
+	  )))
+    (inputs (list gcc-toolchain))
+    (native-inputs
+     (list tar
+	   zstd
+	   ))
+
+    (home-page "https://ollama.ai")
+    (synopsis "Run large language models locally")
+    (description
+     "Ollama is a tool for running large language models locally. It bundles
+model weights, configuration, and data into a single package. It
+optimizes setup and configuration details, including GPU usage.")
+    (license license:expat)))
